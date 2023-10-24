4c31253bd88dcb1a3d24baf4c10c0aea3b347b1e
Jack <jf20@sanger.ac.uk>
2023-10-24 11:10:27 +0100

    Corrected some file naming issues

--- launcher.sh
+++ launcher.sh
@@ -11,8 +11,9 @@ for thin in 0 0.1 0.5 0.9 0.99 0.999 0.9999; do
 	for gamma in 0.1 1 3 5 10; do
 		for i in "${!files[@]}"; do
 			# echo ${files[i]} " " $thin " " $gamma
-			simple="${thin/.}"
-			./deforest -f ../Coverage/${files[i]}.dat -sigmaResolution 10 -worker 40 -sigmaMin 5 -sigmaMax 30 -o Output/Thin$simple"Gamma"$gamma/${output[i]} -L 1000000 -gamma $gamma -thin 10 -accelerate 50 -Qmax 14 -alpha 0.01  -smooth $thin
+			simpleThin="${thin//.}"
+			simpleGamma="${gamma//.}"
+			echo ./deforest -f ../Coverage/${files[i]}.dat -sigmaResolution 10 -worker 40 -sigmaMin 5 -sigmaMax 30 -o Output/Thin$simpleThin"Gamma"$simpleGamma/${output[i]} -L 1000000 -gamma $gamma -thin 10 -accelerate 50 -Qmax 14 -alpha 0.01  -smooth $thin
 		done
 	done
 done
